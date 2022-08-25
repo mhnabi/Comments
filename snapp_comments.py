0e8e89a5e206d14994549f0f0f6293a934e5d1a5
mhnabi <m.hosein.n@gmail.com>
2022-08-25 18:03:28 +0430

    get Snapp comments from CafeBazaar and get save it

--- snapp_comments.py
+++ snapp_comments.py
@@ -1,16 +1,31 @@
+import re
 import requests
+import json
 
 start = 1
 end = 200
-snapp_text = open('snapp_comments2.json', 'a', encoding="utf-8")
+# snapp_text = open('snapp_comments3.json', 'a+', encoding="utf-8")
 url = 'https://api.cafebazaar.ir/rest-v1/process/ReviewRequest'
 
-for i in range(20):
-    payload = {'properties' : {"language": 2, "clientID": "hot0ydqh4cw5heubx6gqzkk9y8f8uxfh", "deviceID": "hot0ydqh4cw5heubx6gqzkk9y8f8uxfh", "clientVersion": "web"
-}, 'singleRequest': {"reviewRequest": {"packageName": "cab.snapp.passenger", "start": start, "end": end} } }
-    res = requests.post(url, json = payload)
+comments = ["id", "date", "versionCode", "rate", "likes", "comment"]
+for i in range(30):
+    payload = {'properties': {"language": 2, "clientID": "hot0ydqh4cw5heubx6gqzkk9y8f8uxfh", "deviceID": "hot0ydqh4cw5heubx6gqzkk9y8f8uxfh", "clientVersion": "web"
+                              }, 'singleRequest': {"reviewRequest": {"packageName": "cab.snapp.passenger", "start": start, "end": end}}}
+    res = requests.post(url, json=payload)
     start = start + 200
     end = end + 200
-    snapp_text.write(res.text)
+    res_json = json.loads(res.text)
     print(res.status_code)
-snapp_text.close()
+    for j in range(end-start):
+        comments.append(res_json['singleReply']['reviewReply']['reviews'][j]['id'])
+        comments.append(res_json['singleReply']['reviewReply']['reviews'][j]['date'])
+        comments.append(res_json['singleReply']['reviewReply']['reviews'][j]['versionCode'])
+        comments.append(res_json['singleReply']['reviewReply']['reviews'][j]['rate'])
+        comments.append(res_json['singleReply']['reviewReply']['reviews'][j]['likes'])
+        comments.append(res_json['singleReply']['reviewReply']['reviews'][j]['comment'])
+
+with open('comments2.csv', 'a', encoding="utf-8") as f:
+    f.write(str(comments))
+print(len(comments))
+
+# snapp_text.write(file)
